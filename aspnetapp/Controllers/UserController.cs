9f050352c60fdc6a0b54093395f834fc4c272e76
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-09-21 00:28:56 +0800

    [修复] 修复命名+创建文件

---
+++ aspnetapp/Controllers/UserController.cs
@@ -0,0 +1,4 @@
+﻿namespace aspnetapp.Controllers {
+    public class UserController {
+    }
+}
--- aspnetapp/Models/Vehicles.cs
+++ aspnetapp/Models/Vehicles.cs
@@ -1,7 +1,7 @@
 ﻿namespace aspnetapp.Models {
     public class Vehicles {
         private int _vehicleId;
-        private States _state;
+        private Estates _state;
         private string? _type = null;
 
         public int VehicleId {
@@ -12,12 +12,12 @@
             set => _type = value;
             get => _type;
         }
-        public States State {
+        public Estates State {
             get {
                 return _state;
             }
             set {
-                if (States.IsDefined(value)) {
+                if (Estates.IsDefined(value)) {
                     _state = value;
                 } else {
                     throw new Exception("State 状态值无效");
@@ -25,7 +25,7 @@
             }
         }
 
-        public enum States {
+        public enum Estates {
             IDLE,// 空闲
             LEASED,// 已出租
             CHARGING,// 充电中
