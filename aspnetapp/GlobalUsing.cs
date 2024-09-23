a45c6d641a27240e42ff276bd5c557fb34e564c8
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-09-23 12:40:15 +0800

    创建文件
    GlobalUsing.cs

---
+++ aspnetapp/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Senparc.NeuChar.MessageHandlers;
+global using Senparc.Weixin.AspNet;
+global using Senparc.Weixin.WxOpen;
+global using Senparc.Weixin.WxOpen.MessageHandlers;
+global using Senparc.Weixin.RegisterServices;
+global using Microsoft.Extensions.Options;
+global using Senparc.Weixin.Entities;
+global using Microsoft.Extensions.FileProviders;
