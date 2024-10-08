049ac558055ed43dbf9b78e2f6241b351bcf2175
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-10-09 01:00:07 +0800

    [修改] 数据库连接

--- aspnetapp/Dao/DatabaseConfig.cs
+++ aspnetapp/Dao/DatabaseConfig.cs
@@ -11,7 +11,7 @@
             var addressParts = Environment.GetEnvironmentVariable("10.28.103.78:3306")?.Split(':');// 更改为全局变量
             var host = addressParts?[0];
             var port = addressParts?[1];
-            return $"server={host};port={port};user={username};password={password};database=aspnet_demo";//
+            return $"server={host};port={port};user={username};password={password};database=wxcloudrun-dotnet-001";//
         }
         /// <summary>
         /// 连接数据库
