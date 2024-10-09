cd49dbd7c29c6b0315abdac9748610859cb71a25
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-10-09 21:52:10 +0800

    [更新] 连接数据库并初始化表

--- aspnetapp/Dao/MyDbContext.cs
+++ aspnetapp/Dao/MyDbContext.cs
@@ -2,21 +2,24 @@
     public partial class MyDbContext : DbContext {
         public MyDbContext() { }
 
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
         public DbSet<User> User { get; set; } = null!;
         public DbSet<Store> Store { get; set; } = null!;
         public DbSet<Vehicle> Vehicle { get; set; } = null!;
         public DbSet<Order> Order { get; set; } = null!;
-        public DbSet<HomepageAd> homepageAds { get; set; } = null!;
-
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<HomepageAd> HomepageAds { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // 配置数据库连接
             DatabaseConfig.ConfigureMySql(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            // 设置默认字符集和排序规则
             modelBuilder.UseCollation("utf8_general_ci").HasCharSet("utf8");
-            //modelBuilder.Entity<User>().ToTable("Users");// 数据库表名
+
+            // 额外的模型配置
             OnModelCreatingPartial(modelBuilder);
         }
 
---
+++ aspnetapp/DatabaseConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace aspnetapp {
+    // 数据库基本连接
+    public class DatabaseConfig {
+        /// <summary>
+        /// 获取数据库连接字符串
+        /// </summary>
+        /// <returns>数据库连接字符串</returns>
+        public static string GetConnectionString() {
+            // 获取环境变量中的数据库配置
+            var username = Environment.GetEnvironmentVariable("DB_USERNAME") ?? "root";
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "Abc*123*";
+            var host = Environment.GetEnvironmentVariable("DB_HOST") ?? "sh-cynosdbmysql-grp-owvd31mk.sql.tencentcdb.com";
+            var port = Environment.GetEnvironmentVariable("DB_PORT") ?? "26679";
+            var database = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "wxcloudrun-dotnet-001";
+
+            // 输出一些调试信息
+            Console.WriteLine($"host: {host}, port: {port}, username: {username}");
+
+            // 返回连接字符串
+            return $"server={host};port={port};user={username};password={password};database={database}";
+        }
+
+        /// <summary>
+        /// 连接数据库
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        public static void ConfigureMySql(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                var connStr = GetConnectionString();
+                optionsBuilder.UseMySql(connStr, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0-mysql"));
+            }
+        }
+    }
+}
