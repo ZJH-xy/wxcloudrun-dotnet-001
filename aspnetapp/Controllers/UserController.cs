47303d378c79856772e06399179ba2ecfe06740e
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-10-16 20:07:16 +0800

    [更新] api
    用户、门店

--- aspnetapp/Controllers/UserController.cs
+++ aspnetapp/Controllers/UserController.cs
@@ -7,10 +7,15 @@
             _context = context;
         }
 
+        public DbContext GetContext() {
+            return _context;
+        }
+
         public async Task<int> AddUser(User user) {
             await _context.User.AddAsync(user);
             return await _context.SaveChangesAsync();
         }
+
         public async Task<User?> GetUser(int id) {
             User? user = await _context.User.SingleOrDefaultAsync(u => u.UserId == id);
             return user;
