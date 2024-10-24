0634c4838565e8264980e27644327296d3010331
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-10-24 23:33:58 +0800

    [修复] 传递参数

--- aspnetapp/Controllers/API/UserAPI.cs
+++ aspnetapp/Controllers/API/UserAPI.cs
@@ -51,7 +51,7 @@ namespace aspnetapp.Controllers.API {
 
         // 获取用户所有信息
         [HttpGet("a/{id}")]
-        public async Task<IActionResult> GetUserPro(int id, [FromBody] GetPassword data) {
+        public async Task<IActionResult> GetUserPro(int id, GetPassword data) {
             if (data.Password is null)
                 return StatusCode(403, "密码为空");
 
@@ -83,7 +83,7 @@ namespace aspnetapp.Controllers.API {
 
         // 获取用户所有信息
         [HttpGet("l/{phone}")]
-        public async Task<IActionResult> GetUserproByPhone(string phone, [FromBody] GetPassword data) {
+        public async Task<IActionResult> GetUserproByPhone(string phone, GetPassword data) {
             if (data.Password is null)
                 return StatusCode(403, "密码为空");
 
