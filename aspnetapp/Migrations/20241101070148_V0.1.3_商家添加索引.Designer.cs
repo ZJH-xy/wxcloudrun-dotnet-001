fcfe2274b3c35ee72e3e47ec46b474dc05faa401
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-11-01 15:04:46 +0800

    [更新] 商家添加索引

--- aspnetapp/Migrations/20241101070148_V0.1.3_商家添加索引.Designer.cs
+++ aspnetapp/Migrations/20241101070148_V0.1.3_商家添加索引.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aspnetapp.Dao;
 
@@ -10,9 +11,10 @@ using aspnetapp.Dao;
 namespace aspnetapp.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241101070148_V0.1.3_商家添加索引")]
+    partial class V013_商家添加索引
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
