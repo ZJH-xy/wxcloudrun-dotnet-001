7de13ff5a83d727a918b1f06295c7f7468fdbb69
1162200133 <131670402+1162200133@users.noreply.github.com>
2024-11-22 18:54:35 +0800

    [更新]加载动画、点击加载图片

---
+++ aspnetapp/wwwroot/js/loading.js
@@ -0,0 +1,16 @@
+﻿// 显示加载中
+async function loadHTML() {
+	document.getElementById('loadding').style.display = 'block';
+	setTimeout(() => {
+		cancelloadHTML();
+	}, 10000);
+
+}
+// 取消显示加载中
+function cancelloadHTML() {
+	document.getElementById('loadding').style.display = 'none';
+}
+
+setTimeout(() => {
+	cancelloadHTML();
+}, 10000);
