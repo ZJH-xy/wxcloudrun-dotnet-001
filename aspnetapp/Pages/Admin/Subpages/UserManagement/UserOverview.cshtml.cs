06db255d21da0fc6dcc7fe08df8ab1149890102e
1162200133 <131670402+1162200133@users.noreply.github.com>
2024-11-23 20:33:21 +0800

    [更新]页码

--- aspnetapp/Pages/Admin/Subpages/UserManagement/UserOverview.cshtml.cs
+++ aspnetapp/Pages/Admin/Subpages/UserManagement/UserOverview.cshtml.cs
@@ -42,9 +42,8 @@ namespace aspnetapp.Pages.Admin.Subpages.UserManagement {
 
         [BindProperty(SupportsGet = true)]
 		public static int PageIndex { get; set; } = 1;
-
-        // 更新
-        [BindProperty]
+		// 更新
+		[BindProperty]
         public User UpdatedUser { get; set; } = new User();
 
         // 查询
@@ -197,7 +196,7 @@ namespace aspnetapp.Pages.Admin.Subpages.UserManagement {
 
             PageIndex = requestData["PageIndex"];
 
-			return new JsonResult(new { success = true, message = "成功"});
+			return new JsonResult(new { success = true, message = "成功", pageIndex = PageIndex });
 		}
 
 		/// <summary>
