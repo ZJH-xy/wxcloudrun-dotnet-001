a69a7709e97ab72280d66000a7e20dbc1b091bb0
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-12-06 15:33:48 +0800

    [修改] 用户表删除Unionid

---
+++ aspnetapp/Migrations/20241206073247_V0.2.6_用户表删除Unionid.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    public partial class V026_用户表删除Unionid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unionid",
+                table: "T_Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Unionid",
+                table: "T_Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
