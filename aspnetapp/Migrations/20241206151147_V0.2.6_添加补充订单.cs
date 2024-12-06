70f1571d592180bc624d503810666a9aeec52987
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-12-06 23:12:44 +0800

    [更新] 添加补充订单

---
+++ aspnetapp/Migrations/20241206151147_V0.2.6_添加补充订单.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    public partial class V026_添加补充订单 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupplementaryOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    OutTradeNo = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TransactionId = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Total = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Paid = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    SuccessTime = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    RowVersion = table.Column<DateTime>(type: "timestamp(6)", rowVersion: true, nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupplementaryOrders", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupplementaryOrders");
+        }
+    }
+}
---
+++ aspnetapp/Models/SupplementaryOrders.cs
@@ -0,0 +1,57 @@
+﻿using static aspnetapp.Models.Order;
+
+namespace aspnetapp.Models {
+    /// <summary>
+    /// 补充订单
+    /// </summary>
+    public class SupplementaryOrders {
+        /// <summary>
+        /// 补充订单编号
+        /// </summary>
+        [Key]
+        public int Id { get; init; }
+        /// <summary>
+        /// 商户系统内部订单号，可以是数字、大小写字母_-*的任意组合且在同一个商户号下唯一。
+        /// </summary>
+        public string? OutTradeNo { get; set; }
+
+        /// <summary>
+        /// 微信支付系统生成的订单号
+        /// </summary>
+        public string? TransactionId { get; set; }
+        /// <summary>
+        /// 【总金额】订单总金额
+        /// </summary>
+        [Precision(10, 2)]
+        public decimal Total { get; set; } = 0;
+        /// <summary>
+        /// 已付
+        /// </summary>
+        [Precision(10, 2)]
+        public decimal Paid { get; set; } = 0;
+        /// <summary>
+		/// 订单状态
+		/// </summary>
+		public EOrderStatus Status { get; set; }
+        /// <summary>
+		/// 支付完成时间（微信获取）
+		/// </summary>
+		public DateTime? SuccessTime { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+
+        /// <summary>
+        /// 用于乐观并发控制
+        /// </summary>
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
