2b312cc72304f16676eced4661419ee59189e12e
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-12-08 23:05:46 +0800

    补余退款

--- aspnetapp/Controllers/API/Admin/RefundOrderAPI.cs
+++ aspnetapp/Controllers/API/Admin/RefundOrderAPI.cs
@@ -1,4 +1,5 @@
 ﻿using aspnetapp.Controllers.API.Miniprogram;
+using Microsoft.EntityFrameworkCore;
 using Senparc.Weixin.TenPayV3.Apis.BasePay;
 
 namespace aspnetapp.Controllers.API.Admin {
@@ -56,6 +57,10 @@ namespace aspnetapp.Controllers.API.Admin {
 				// 调用退款服务
 				RefundReturnJson refundReturnJson = await OrderAPI.RefundAsync(order, refundOrder);
 
+				if (refundReturnJson.ResultCode.Success != true) {
+					return StatusCode(403, refundReturnJson.ToJson());
+				}
+
 				// 生成退款表数据
 				_dbContext.RefundOrder.Add(refundOrder);
 				await _dbContext.SaveChangesAsync();
@@ -63,8 +68,67 @@ namespace aspnetapp.Controllers.API.Admin {
 				// 如果退款成功，提交事务
 				await transaction.CommitAsync();
 			} catch (Exception e) {
-				_logger.LogError(e, "订单处理失败。订单ID: {OrderId}, 车辆ID: {VehicleId}, 退款金额: {RefundAmount}",
-					order.Id, order.TheVehicle, refundOrder.Refund);
+				await transaction.RollbackAsync();
+
+				return StatusCode(500, e);
+			}
+			return StatusCode(200);
+		}
+
+		/// <summary>
+		/// 补余退款
+		/// </summary>
+		/// <param name="getData"></param>
+		/// <returns></returns>
+		[HttpPost("refund")]
+		public async Task<IActionResult> RefundSupplementaryOrders(GetData1 getData) {
+			SupplementaryOrders? supplementaryOrders;
+			try {
+				supplementaryOrders = await _dbContext.SupplementaryOrders.SingleOrDefaultAsync(o => o.Id == getData.TheOrder);
+
+			} catch (Exception e) {
+				return StatusCode(500, e);
+			}
+
+			if (supplementaryOrders == null)
+				return StatusCode(404, "未找到订单");
+
+			var now = DateTime.Now;
+
+			// 新建退款表数据
+			RefundOrder refundOrder = new() {
+				TheOrder = supplementaryOrders.Id,
+				//outRefundNo = string.Concat("Refund_", Guid.NewGuid().ToString("N").AsSpan(0, 20)),
+				RefundId = "",//等待
+				Reason = "管理员补余退款",
+				Status = RefundOrder.Estatus.已创建,//等待
+				Total = supplementaryOrders.Paid,
+				Refund = getData.Refund,//退还费用
+				SuccessTime = null,//等待
+				CreateTime = now,//等待
+				UpdatedAt = now,
+			};
+
+			using var transaction = await _dbContext.Database.BeginTransactionAsync();// 事务开始
+
+			try {
+				// 调用退款服务
+				RefundReturnJson refundReturnJson = await OrderAPI.RefundAsync(new Order {
+					TransactionId = supplementaryOrders.TransactionId,
+					OutTradeNo = supplementaryOrders.OutTradeNo
+				}, refundOrder);
+
+				if (refundReturnJson.ResultCode.Success != true) {
+					return StatusCode(403, refundReturnJson.ToJson());
+				}
+
+				// 生成退款表数据
+				_dbContext.RefundOrder.Add(refundOrder);
+				await _dbContext.SaveChangesAsync();
+
+				// 如果退款成功，提交事务
+				await transaction.CommitAsync();
+			} catch (Exception e) {
 				await transaction.RollbackAsync();
 
 				return StatusCode(500, e);
@@ -72,6 +136,7 @@ namespace aspnetapp.Controllers.API.Admin {
 			return StatusCode(200);
 		}
 	}
+}
 
 	public class GetData1 {
 		/// <summary>
