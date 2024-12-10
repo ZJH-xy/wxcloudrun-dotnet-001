508c1c21d1c6ec656e744c694ecc1ede16b8bde9
周家辉 <3107154372@qq.com>
2024-12-10 19:41:39 +0800

    [更新] 添加门店套餐表

---
+++ aspnetapp/Pages/Admin/Subpages/StoreManagement/StoreMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetapp.Pages.Admin.Subpages.StoreManagement
+{
+    public class StoreMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
