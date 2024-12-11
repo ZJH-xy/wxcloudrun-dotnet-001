a0dbbb68285dc92e68866179bdeca02bdabe7a3d
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-12-11 21:03:22 +0800

    [更新] 新建公告表、广告表、审核商家地址表

--- aspnetapp/Migrations/20241211130225_V0.2.9_新建公告表、广告表、审核商家地址表.Designer.cs
+++ aspnetapp/Migrations/20241211130225_V0.2.9_新建公告表、广告表、审核商家地址表.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace aspnetapp.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241211130225_V0.2.9_新建公告表、广告表、审核商家地址表")]
+    partial class V029_新建公告表广告表审核商家地址表
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
