2fdbba186b88371f31fe0459487135f86d323d92
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2024-12-12 20:47:48 +0800

    [更新] 广告添加图片链接

--- aspnetapp/Migrations/20241212124642_V0.2.11_广告添加图片链接.Designer.cs
+++ aspnetapp/Migrations/20241212124642_V0.2.11_广告添加图片链接.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace aspnetapp.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241212124642_V0.2.11_广告添加图片链接")]
+    partial class V0211_广告添加图片链接
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
