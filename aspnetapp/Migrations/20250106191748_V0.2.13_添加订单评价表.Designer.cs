acb6b51a7e2955cf0f99d551dddbf61e42bda484
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2025-01-07 03:20:40 +0800

    [添加] 订单评价表

--- aspnetapp/Migrations/20250106191748_V0.2.13_添加订单评价表.Designer.cs
+++ aspnetapp/Migrations/20250106191748_V0.2.13_添加订单评价表.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace aspnetapp.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250106191748_V0.2.13_添加订单评价表")]
+    partial class V0213_添加订单评价表
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnetapp/Models/OrderEvaluate.cs
@@ -0,0 +1,38 @@
+﻿namespace aspnetapp.Models {
+	/// <summary>
+	/// 订单评价表
+	/// </summary>
+	public class OrderEvaluate {
+		/// <summary>
+		/// 订单评价编号
+		/// </summary>
+		[Key]
+		public int Id { get; init; }
+
+		/// <summary>
+		/// 订单编号
+		/// </summary>
+		public int TheOrder { get; set; }
+
+		/// <summary>
+		/// 用户评分（1-10分）
+		/// </summary>
+		public int Score { get; set; }
+
+		/// <summary>
+		/// 备注
+		/// </summary>
+		public string? Notes { get; set; }
+
+		/// <summary>
+		/// 创建时间
+		/// </summary>
+		public DateTime CreatedAt { get; set; }
+
+		/// <summary>
+		/// 用于乐观并发控制
+		/// </summary>
+		[Timestamp]
+		public byte[] RowVersion { get; set; }
+	}
+}
