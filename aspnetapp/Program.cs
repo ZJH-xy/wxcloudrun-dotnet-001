edcc1722b49f0670c45b15acd535adbad7abeb14
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2025-01-08 11:55:01 +0800

    [更新] web后台登录

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -45,6 +45,7 @@ builder.Services.AddScoped<StoreMenuControllerWeb>();
 builder.Services.AddScoped<StoreAccountControllerWeb>();
 builder.Services.AddScoped<AdvertisementControllerWeb>();
 builder.Services.AddScoped<NoticeControllerWeb>();
+builder.Services.AddScoped<LoginControllerWeb>();
 
 builder.Services.AddScoped<OrderAPI>(); // 注册 OrderAPI，定时取消订单
 builder.Services.AddHostedService<TimedHostedService>();
