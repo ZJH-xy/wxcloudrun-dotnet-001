03f2bb53d8be0656975a97d474bb0f6cc8e23d03
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2025-01-08 19:39:37 +0800

    [更新] 商家帐号唯一

---
+++ aspnetapp/Migrations/20250108112725_V0.2.14_商家帐号唯一索引.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    public partial class V0214_商家帐号唯一索引 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_T_StoreAccount_TheStore_Account",
+                table: "T_StoreAccount");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_StoreAccount_Account",
+                table: "T_StoreAccount",
+                column: "Account",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_T_StoreAccount_Account",
+                table: "T_StoreAccount");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_StoreAccount_TheStore_Account",
+                table: "T_StoreAccount",
+                columns: new[] { "TheStore", "Account" },
+                unique: true);
+        }
+    }
+}
