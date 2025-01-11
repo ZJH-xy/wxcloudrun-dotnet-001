f45dcfe18e1831af49ff26fa3a1105bc1bee3509
1162200133 <1162200133@qq.com>
2025-01-11 17:05:17 +0800

    [更新]添加车辆更改记录、优化页面切换逻辑

---
+++ aspnetapp/Pages/Admin/Subpages/OrderManagement/VehicleReplacementRecord.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetapp.Pages.Admin.Subpages.OrderManagement
+{
+    public class VehicleReplacementRecordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
