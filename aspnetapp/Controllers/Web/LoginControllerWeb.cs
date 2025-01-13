d37188eeb8a70f49a1411863681dc3d1216bf5c8
ZJH-xy <119393310+ZJH-xy@users.noreply.github.com>
2025-01-14 01:22:26 +0800

    [更新] 管理员登录jwt添加

--- aspnetapp/Controllers/Web/LoginControllerWeb.cs
+++ aspnetapp/Controllers/Web/LoginControllerWeb.cs
@@ -1,25 +1,113 @@
-﻿namespace aspnetapp.Controllers.Web {
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace aspnetapp.Controllers.Web {
 	public class LoginControllerWeb : Controller {
 		private readonly MyDbContext _context;
 		private readonly ILogger<LoginControllerWeb> _logger;
+		private readonly IOptionsSnapshot<JWTSettings> _JWTSettingsOpt;
 
-		public LoginControllerWeb(MyDbContext context, ILogger<LoginControllerWeb> logger) {
+		public LoginControllerWeb(MyDbContext context, IOptionsSnapshot<JWTSettings> jWTSettingsOpt, ILogger<LoginControllerWeb> logger) {
 			_context = context;
+			_JWTSettingsOpt = jWTSettingsOpt;
 			_logger = logger;
 		}
 
+		/// <summary>
+		/// 登录判断
+		/// </summary>
+		/// <param name="account"></param>
+		/// <param name="password"></param>
+		/// <returns>真登录成功</returns>
+		public async Task<(bool, string)> LoginAsync(string account, string password) {
+			var adminAccount = await _context.AdminAccount.SingleOrDefaultAsync(a => a.Account == account);
 
-		public bool Login(string account, string password) {
-			var aa = _context.AdminAccount.SingleOrDefault(a => a.Account == account);
-			if (aa is null) {
-				return false;
+			if (adminAccount is null) {
+				return (false, "");
 			}
 
-			if (aa.Password == password) {
-				return true;
+			if (adminAccount.Password == password) {
+				return (true, GetJwtToken(CreateClaim(adminAccount.Id.ToString(), "admin")));
 			}
 
-			return false;
+			return (false, "");
+		}
+
+		/// <summary>
+		/// 创建Claim
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="role">角色</param>
+		/// <returns>List<Claim> 对象</returns>
+		private List<Claim> CreateClaim(string id, string role) {
+			List<Claim> claims = new() {
+				new Claim(ClaimTypes.NameIdentifier, id),
+				new Claim(ClaimTypes.Role, role)
+			};
+			return claims;
+		}
+
+		/// <summary>
+		/// JWT 令牌计算
+		/// </summary>
+		/// <param name="claims"></param>
+		/// <returns></returns>
+		private string GetJwtToken(List<Claim> claims) {
+			// 读取配置
+			string key = _JWTSettingsOpt.Value.SecKey;
+			DateTime expires = DateTime.Now.AddDays(_JWTSettingsOpt.Value.ExpireDays);// 读取配置过期时间
+
+			// 计算
+			byte[] secBytes = Encoding.UTF8.GetBytes(key);
+			var secKey = new SymmetricSecurityKey(secBytes);
+			var credentials = new SigningCredentials(secKey, SecurityAlgorithms.HmacSha256Signature);
+			var tokenDescriptor = new JwtSecurityToken(claims: claims,
+				expires: expires, signingCredentials: credentials);
+			string jwt = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+			_logger.LogDebug("角色：{Role}，ID：{NameIdentifier}生成新JWTtoken：{claims}",
+				claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value,
+				claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+				claims.ToJToken());
+			return jwt;
+		}
+
+		/// <summary>
+		/// 验证密码
+		/// </summary>
+		/// <param name="password"></param>
+		/// <param name="hashedPassword"></param>
+		/// <returns>等于true，否则false</returns>
+		private static bool VerifyPassword(string password, string hashedPassword) {
+			// 对输入的密码进行SHA-256哈希
+			//string hashedInputPassword = HashPassword(password);
+			string hashedInputPassword = password;
+
+			return hashedInputPassword == hashedPassword;
+		}
+
+		/// <summary>
+		/// 使用SHA-256哈希密码
+		/// </summary>
+		/// <param name="password"></param>
+		/// <returns></returns>
+		private static string HashPassword(string password) {
+			using (SHA256 sha256 = SHA256.Create()) {
+				// 将输入密码转换为字节数组
+				byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+				// 计算哈希值
+				byte[] hashBytes = sha256.ComputeHash(bytes);
+
+				// 将哈希字节数组转换为16进制字符串
+				StringBuilder builder = new StringBuilder();
+				foreach (var b in hashBytes) {
+					builder.Append(b.ToString("x2"));
+				}
+
+				// 返回哈希后的密码
+				return builder.ToString();
+			}
 		}
 	}
 }
--- aspnetapp/Pages/Admin/login.cshtml.cs
+++ aspnetapp/Pages/Admin/login.cshtml.cs
@@ -2,6 +2,7 @@
 using aspnetapp.Pages.Admin.Subpages.StoreManagement;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace aspnetapp.Pages.Admin {
 	public class LoginModel : PageModel {
@@ -21,20 +22,38 @@ namespace aspnetapp.Pages.Admin {
 
 		public string ErrorMessage { get; set; }
 
-		public IActionResult OnGet() {
+		public IActionResult OnGetAsync() {
 			return Page();
 		}
 
-		public IActionResult OnPost() {
+		public async Task<IActionResult> OnPostAsync() {
 			// 处理 POST 请求
-			if (_loginControllerWeb.Login(Username, Password)) {
-				// 登录成功，跳转到主页或其他页面
-				return RedirectToPage("/admin/main");
+			var re = await _loginControllerWeb.LoginAsync(Username, Password);
+
+			if (re.Item1) {
+				// 登录成功
+				return StatusCode(200, new{ JWT = re.Item2 });
+
+				// 跳转到主页或其他页面
+				//return RedirectToPage("/admin/main");
 			} else {
 				// 登录失败，显示错误信息
 				ErrorMessage = "用户名或密码错误";
 				return Page();
 			}
+		}
+
+		/// <summary>
+		/// JWT 获取用户id
+		/// </summary>
+		/// <returns></returns>
+		public int GetUserIdInt() {
+			return int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+		}
+
+		// JWT 获取用户id
+		public string GetUserIdString() {
+			return User.FindFirstValue(ClaimTypes.NameIdentifier);
 		}
 	}
 }
