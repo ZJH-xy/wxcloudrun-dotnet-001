5fc8b9e9ef83aa4f4c3d147acec7874d1a33ca9a
1162200133 <1162200133@qq.com>
2025-01-16 02:55:50 +0800

    [更新] 登录

--- aspnetapp/Pages/Admin/Main.cshtml.cs
+++ aspnetapp/Pages/Admin/Main.cshtml.cs
@@ -4,16 +4,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace aspnetapp.Pages.Admin
 {
     public class homeModel : PageModel{
-   //     public int OnGet(){
-			//// 读取名为 "JWT" 的 Cookie
-			//if (Request.Cookies.TryGetValue("JWT", out var jwt)){
-			//	Console.WriteLine($"JWT: {jwt}");
-			//	if(jwt == null){
-			//		return Page("/admin/login");
+  //      public int OnGet(){
+		//	// 读取名为 "JWT" 的 Cookie
+		//	if (Request.Cookies.TryGetValue("JWT", out var jwt)){
+		//		Console.WriteLine($"JWT: {jwt}");
+		//		if(jwt == null){
+		//			return Page("/admin/login");
 
-			//	}
-			//}
-		}
+		//		}
+		//	}
+		//}
 
     }
 }
